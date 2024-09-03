a4cf435fb879f856f9b0618ffd0c739c01361fed
Sunstrophe <mackansund_2@live.se>
2024-09-04 00:54:07 +0200

    Added whispermodel and changed numpy ver

--- webservice/backend/main.py
+++ webservice/backend/main.py
@@ -1,13 +1,36 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import List
+import whispermodel
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connection: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connection.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connection.remove(websocket)
+
+    async def broadcast(self, data: bytes):
+        # Use the datastream to send to our LLM.
+        # await connection.send_bytes(data)
+        whispermodel.process_audio(data)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/audio")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_bytes()
+            await manager.broadcast(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
---
+++ webservice/backend/whispermodel.py
@@ -0,0 +1,47 @@
+import whisper
+import io
+import wave
+
+MINIMUM_SIZE_FOR_PROCESSING = 32000
+
+model = whisper.load_model("tiny")
+
+
+async def process_audio(datastream):
+
+    audio_buffer = io.BytesIO()
+
+    async for message in datastream:
+        audio_buffer.write(message)
+
+        if enough_data(audio_buffer):
+            wav_data = convert_to_wav(audio_buffer)
+            transcription = transcribe_audio(wav_data)
+            print(transcription)
+            await datastream.send(transcription)
+
+            audio_buffer = io.BytesIO()
+
+
+def enough_data(buffer):
+    return buffer.tell() > MINIMUM_SIZE_FOR_PROCESSING
+
+
+def convert_to_wav(buffer):
+    buffer.seek(0)
+    with wave.open(io.BytesIO(), 'wb') as wf:
+        wf.setnchannels(1)  # Mono
+        wf.setsampwidth(2)  # Sample width in bytes
+        wf.setframerate(16000)  # Sampling rate
+        wf.writeframes(buffer.read())
+    # Return the WAV data as a bytes-like object
+    return io.BytesIO(wf.readframes())
+
+
+def transcribe_audio(wav_data):
+    result = model.transcribe(wav_data)
+    return result['text']
+
+
+# if __name__ == "__main__":
+#     transcribe_audio()
